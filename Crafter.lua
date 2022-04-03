77e4e1b661b398ce527b3b1dcd8a9bb2eccaf484
Sharlikran <danielhmpdx16@gmail.com>
2022-04-02 23:20:36 -0700

    Version: 3.0.96.6

--- Crafter.lua
+++ Crafter.lua
@@ -679,7 +679,7 @@ local showOnce= true
 local updateWarningShown = false
 local function craftCheck(eventcode, station)
 
-	local currentAPIVersionOfAddon = 101032
+	local currentAPIVersionOfAddon = GetAPIVersion()
 
 	if GetAPIVersion() > currentAPIVersionOfAddon and GetWorldName()~="PTS" and not updateWarningShown then
 		d("Update your addons!")
